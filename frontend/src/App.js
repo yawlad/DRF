b3390e0b51b8d0b0df0348dc155ee1e3b887d67a
yawlad <y.yawlad@gmail.com>
2022-10-10 20:07:57 +0300

    Completed 2 task

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import UserList from "./components/User";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
+import axios from "axios";
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      'users': [],
+
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://127.0.0.1:8000/api/users/').then(response => {
+      const users = response.data
+      this.setState( {
+        'users': users
+      })
+    }).catch(error => {
+      console.log(error)
+    })
+    
+  }
+
+
+
+  render() {
+    return (
+   <div>
+      <UserList users={this.state.users}/>
+   </div>
+  )
+  }
+}
+
+export default App;
